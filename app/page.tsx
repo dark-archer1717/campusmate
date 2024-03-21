37d48b8d9f1555551ddbed1dd187f299f893d81e
Kazi Efazul Karim <efazul.fayad1717@gmail.com>
2024-03-22 02:49:48 +0600

    Refactor HeroSection import

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 
 import React from 'react';
 import Navbar from './../components/nav';
-import HeroSection from '@/components/HeroSectionHeroSection';
+import HeroSection from '@/components/HeroSection';
 
 export default function Home() {
   return (
