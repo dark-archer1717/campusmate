723d2e686f90324ebec3979c13bf28a5d858def6
Kazi Efazul Karim <efazul.fayad1717@gmail.com>
2024-03-22 02:56:31 +0600

    Add Image component to NavBar

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,13 +1,14 @@
 // components/NavBar.tsx
 
 import React from 'react';
+import Image from 'next/image';
 
 const NavBar: React.FC = () => {
   return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="SEU Campusmate Logo" />
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <Image src="/public/seu.png" width="32" height="32" className="w-8" alt="SEU Campusmate Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SEU Campusmate</span>
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
