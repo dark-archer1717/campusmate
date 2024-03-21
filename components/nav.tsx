deb3ab0581da4ee85a9e471990aa34de19520b57
Kazi Efazul Karim <efazul.fayad1717@gmail.com>
2024-03-22 02:59:31 +0600

    Update logo and button text in nav component

--- components/nav.tsx
+++ components/nav.tsx
@@ -8,7 +8,7 @@ const NavBar: React.FC = () => {
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <Image src="/public/seu.png" width="32" height="32" className="w-8" alt="SEU Campusmate Logo" />
+          <Image src="/seu.png" width="32" height="32" className="w-8" alt="SEU Campusmate Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SEU Campusmate</span>
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
@@ -16,7 +16,7 @@ const NavBar: React.FC = () => {
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            Get started
+            Blood Donation
           </button>
           <button
             data-collapse-toggle="navbar-sticky"
@@ -33,14 +33,6 @@ const NavBar: React.FC = () => {
         </div>
         <div className="hidden md:flex items-center justify-between w-full md:w-auto md:order-1" id="navbar-sticky">
           <ul className="flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:p-0 bg-gray-50 dark:bg-gray-800 rounded-lg border border-gray-100 md:bg-transparent md:border-0">
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-blue-700 rounded md:p-0 dark:text-blue-500 md:hover:bg-transparent hover:text-blue-700 dark:hover:text-blue-500"
-              >
-                Home
-              </a>
-            </li>
             <li>
               <a
                 href="#"
